51212b858aefe0b1d7ae2704ec9b0b42789e6a23
ZAMSOFTS <123354162+ZAMSOFTS@users.noreply.github.com>
2025-08-25 17:19:30 +0400

    changed seed code format

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,23 +2,22 @@ const { PrismaClient } = require("../src/generated/prisma");
 const prisma = new PrismaClient();
 
 async function main() {
-  const permissions = ["add:user", "edit:user", "delete:user", "view:user", 
-                      "view:vehicle", "add:vehicle", "edit:vehicle", "delete:vehicle"];
+  const permissions = ["add:user", "edit:user", "delete:user", "view:user", "view:vehicle", "add:vehicle", "edit:vehicle", "delete:vehicle"];
+
+  try {
+    await prisma.permission.deleteMany();
+  } catch {}
 
   for (const p of permissions) {
-   try {
-     await prisma.permission.deleteMany();
-   } catch {}
-   
-   await prisma.permission.upsert({
-     where: { name: p },
-     update: {}, // do nothing if already exists
-     create: { name: p },
-   });
+    await prisma.permission.upsert({
+      where: { name: p },
+      update: {}, // do nothing if already exists
+      create: { name: p },
+    });
   }
 }
 
 main()
   .then(() => console.log("Seed completed ✅"))
-  .catch((e) => console.error(e))
+  .catch(e => console.error(e))
   .finally(() => prisma.$disconnect());
